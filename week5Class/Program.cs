b7a82e0d5f9cc48bc7528998b523b10e169eb82f
Kawsar Ahmed <76034051+sami019325@users.noreply.github.com>
2023-03-18 23:00:12 +0600

    Having an issue
    I cannot access the data of list of list.
    
     List<List<string>>data

--- week5Class/Program.cs
+++ week5Class/Program.cs
@@ -1,8 +1,9 @@
-﻿
+
 
 
 using System.Xml.Linq;
 using System.Collections.Generic;
+using System.Collections;
 
 
 /*Array------------------------------*/
@@ -121,4 +122,75 @@ string command = Console.ReadLine();
     }
 
 }
-addRemoveInList();
+/*addRemoveInList();
+*/
+
+static void arrrangeData()
+{
+    List<List<string>> TableOfLists = new List<List<string>>();
+
+    while (true)
+    {
+        Console.WriteLine("Press 1 to add friends, press 2 to add items, press 3 to exit");
+        int action = int.Parse(Console.ReadLine());
+        if (action == 3)
+        {
+            break;
+        }
+        else if (action == 1)
+        {
+            
+            while (true)
+            {
+                List<string> list = new List<string>();
+
+                Console.WriteLine("Add a friend or press q to exit.");
+                string friend = Console.ReadLine();
+                if (friend== "q")
+                {
+                    break;
+                }
+                else
+                {
+                    list.Add(friend);
+                    continue;
+                }
+            }
+        }
+
+    }
+    Console.WriteLine("the items" );
+    Console.WriteLine(TableOfLists.Count);
+
+
+    /*
+    foreach (List<string> subList in myList)
+{
+    foreach (string item in subList)
+    {
+        Console.WriteLine(item);
+    }
+}
+    
+             */
+
+
+
+    /*  for (int i = 0; i < 3;i++)
+      {
+          for (int x= 0; x < TableOfLists[i].Count;x++)
+          {
+              Console.WriteLine(TableOfLists[i][x].ToString());
+          }
+      }
+      foreach (List<string> lists in TableOfLists)
+      {
+          foreach (string item in lists)
+          {
+              Console.WriteLine(item);
+          }
+      }*/
+
+
+}
+arrrangeData();
